ca0ec3d055fadb7fa3a7f786d2e67fc4ce800ba7
jscoding10 <jscoding10@protonmail.com>
2023-10-21 11:20:06 -0700

    Added new coding challenge

---
+++ 8 kyu/even-or-odd.js
@@ -0,0 +1,16 @@
+// DESCRIPTION:
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// My Solution
+function evenOrOdd(number) {
+    if (number % 2 === 0){
+      return "Even";
+    } else {
+      return "Odd";
+    };
+  };
+
+  // Best Solution
+//   function evenOrOdd(number) {
+//     return number % 2 === 0 ? 'Even' : 'Odd';
+//   }
