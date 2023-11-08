790fbd9ba69ef6a5a631a12f7a9a184b30960292
jscoding10 <jscoding10@protonmail.com>
2023-11-07 19:54:45 -0800

    Added coding challenge

---
+++ 8 kyu/square-n-sum.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 
+
+// My Solution
+function squareSum(numbers){
+    return numbers.reduce((accumulator, current) => accumulator + current * current, 0);
+  }
