fe9815042ba0f36f97bd92805a5ba83576bbfce1
jscoding10 <jscoding10@protonmail.com>
2023-11-14 19:43:59 -0800

    Added new coding challlenge

---
+++ 8 kyu/volume-of-a-cuboid.js
@@ -0,0 +1,9 @@
+// Description
+// Calculate the volume of a cuboid with three values: the length, width and height of the cuboid.
+
+// My Solution
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      return length * width * height;
+    }
+  }
