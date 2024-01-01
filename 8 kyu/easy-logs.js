42c672964c8d9e7cd4ffb69e1ea539a832f7b741
jscoding10 <jscoding10@protonmail.com>
2024-01-01 11:23:15 -0800

    Added new coding challenge

---
+++ 8 kyu/easy-logs.js
@@ -0,0 +1,9 @@
+/*
+Description:
+Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
+*/
+
+// My Solution
+function logs(x , a, b) {
+    return (Math.log(a * b) / Math.log(x));
+}
