8b0ad36c9ec653f2af41a10f6c8e6fea39ee5dad
jscoding10 <jscoding10@protonmail.com>
2024-02-06 06:07:07 -0800

    Added new coding challenge

---
+++ 7 kyu/a-rule-of-divisibility-by-seven.js
@@ -0,0 +1,9 @@
+// My Solution
+const seven = m => {
+    let times = 0
+    while (('' + m).length > 2) {
+      times++
+      m = Math.floor(m / 10) - (m % 10) * 2
+    }
+    return [m, times]
+  }
