b937a91c06d8cc89d02508ac7c3dba54851c063a
jscoding10 <jscoding10@protonmail.com>
2024-02-16 13:17:28 -0800

    Added new coding challenge

---
+++ 7 kyu/averages-of-numbers.js
@@ -0,0 +1,8 @@
+// My Solution
+function averages(numbers) {
+    return  numbers
+        ? numbers
+            .map((number, index, array) => (number + array[index + 1]) / 2)
+            .slice(0, -1)
+        : []
+    }
