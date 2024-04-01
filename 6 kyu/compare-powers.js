319a53d001f8ce75f7e4e9c69ecf6d9792374291
jscoding10 <jscoding10@protonmail.com>
2024-04-01 06:06:31 -0700

    Added new coding challenge

--- 6 kyu/compare-powers.js
+++ 6 kyu/compare-powers.js
@@ -8,11 +8,11 @@ Things tend to get a bit more complicated with both different bases and exponent
 
 Well, by now you have surely guessed that you have to build a function to compare powers, returning -1 if the first member is larger, 0 if they are equal, 1 otherwise; powers to compare will be provided in the [base, exponent] format:
 
-comparePowers([2,10],[2,15])===1
-comparePowers([2,10],[3,10])===1
-comparePowers([2,10],[2,10])===0
-comparePowers([3,9],[5,6])===-1
-comparePowers([7,7],[5,8])===-1
+comparePowers([2,10],[2,15]) === 1
+comparePowers([2,10],[3,10]) === 1
+comparePowers([2,10],[2,10]) === 0
+comparePowers([3,9],[5,6]) === -1
+comparePowers([7,7],[5,8]) === -1
 Only positive integers will be tested, incluing bigger numbers - you are warned now, so be diligent try to implement an efficient solution not to drain too much on CW resources ;)!
 */
 
