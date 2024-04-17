be11b3eefa469f83073854ab99dcb5ccf15c8dc2
jscoding10 <jscoding10@protonmail.com>
2024-04-17 06:06:47 -0700

    added new coding challenge

---
+++ 6 kyu/prime-factors.js
@@ -0,0 +1,11 @@
+// My Solution
+const prime_factors = n => {
+    const factors = [];
+    for (let i = 2; i <= n; i++) {
+      while (n % i == 0) {
+        factors.push(i);
+        n /= i;
+      } 
+    }
+    return factors;
+}
