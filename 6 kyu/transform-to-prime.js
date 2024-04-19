12a917553cc949d59efcff7ab9569c1f01f5c8cc
jscoding10 <jscoding10@protonmail.com>
2024-04-19 10:18:32 -0700

    Added new coding challenge

---
+++ 6 kyu/transform-to-prime.js
@@ -0,0 +1,14 @@
+// My Solution
+function minimumNumber(numbers){
+    let sum= numbers.reduce((a, b) => a + b, 0)
+    for (let i = 0;; i++){
+    if (isPrime(sum + i)){return i}
+    }
+  }
+  function isPrime(n) {
+    let rt = Math.sqrt(n);
+    for(let i = 2; i <= rt; i++) {
+      if(n % i === 0) return false; 
+    }
+    return n !== 1;
+  }
