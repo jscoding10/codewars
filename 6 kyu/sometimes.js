8958070bce6ec1776335b1b57c7a910ae886fb44
jscoding10 <jscoding10@protonmail.com>
2024-04-20 17:22:32 -0700

    Added new coding challenge

---
+++ 6 kyu/sometimes.js
@@ -0,0 +1,12 @@
+// My Solution
+function sometimes(fn) {
+    let i = 0
+    return (a, b)=>{
+    i++
+    if (i < 3){
+    return fn(a, b);
+    }
+    if (i % 2 !== 0) return fn(a, b)
+    if (i % 2 === 0) return `hmm, I don't know!`
+    }
+}
