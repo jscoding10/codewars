a25f630c340b9c5ead23447d1ff10abdc8138f63
jscoding10 <jscoding10@protonmail.com>
2024-04-23 06:07:51 -0700

    Added new coding challenge

---
+++ 6 kyu/the-shell-game.js
@@ -0,0 +1,11 @@
+// My Solution
+find_the_ball=function(start,swaps){
+    swaps.forEach(function (val) {
+          if (val[0] === start) {
+              start = val[1];
+          } else if (val[1] === start) {
+              start = val[0];
+          }
+      });
+      return start;
+}; 
