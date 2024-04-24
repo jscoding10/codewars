f1fa68d9ceb3b260295f4a3d10066491d39f8b67
jscoding10 <jscoding10@protonmail.com>
2024-04-24 06:06:25 -0700

    Added new coding challenge

---
+++ 6 kyu/steps-in-primes.js
@@ -0,0 +1,14 @@
+// My Solution
+function step(g, m, n){
+    for(let i = m; i <= n; i++){
+        if(isPrime(i) && isPrime(i+g)){ return [i, i+g]}
+    }
+    return null;
+}
+function isPrime(n) {
+  let rt = Math.sqrt(n);
+  for(let i = 2; i <= rt; i++) {
+    if(n % i === 0) return false; 
+  }
+  return n !== 1;
+}
