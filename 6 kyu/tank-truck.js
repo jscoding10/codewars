61c09c7036662df75afa25807b7a3b2f85e2e25f
jscoding10 <jscoding10@protonmail.com>
2024-05-12 10:23:05 -0700

    Added new coding challenge

---
+++ 6 kyu/tank-truck.js
@@ -0,0 +1,5 @@
+// My Solution
+function tankvol(h, d, vt) {
+    return Math.floor(((4 * vt)/(Math.pow(d, 2) * Math.PI)) *
+    (Math.pow(d / 2, 2) * Math.acos(1 - h / (d / 2)) - ((d / 2) - h) * Math.sqrt(h * (d - h))))
+}
