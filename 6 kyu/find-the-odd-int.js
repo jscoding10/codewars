7750511a2cd7d13839a0153c5500e5b0ea7d8a91
jscoding10 <jscoding10@protonmail.com>
2024-05-18 17:25:00 -0700

    Added new coding challenge

---
+++ 6 kyu/find-the-odd-int.js
@@ -0,0 +1,9 @@
+// My Solution
+
+function findOdd(A) {
+    let count = {};
+    A.forEach(v => {
+      count[v] = count[v] ? count[v] + 1 : 1;
+    });
+    return + Object.keys(count).find(key => count[key] % 2 === 1);
+}
