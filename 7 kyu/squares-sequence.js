6f611e5199c184e7a002f275823c32f9bbcba7e8
jscoding10 <jscoding10@protonmail.com>
2024-05-28 06:07:08 -0700

    Added new coding challenge

---
+++ 7 kyu/squares-sequence.js
@@ -0,0 +1,12 @@
+// My Solution
+function squares(x, n) {
+    if (n <= 0) return [];
+       let arr=[x];
+         for (let i = 0; i < n - 1; i++)
+         {
+         arr.push(Math.pow(arr[i], 2))
+         }
+         
+       return arr;
+}
+    
