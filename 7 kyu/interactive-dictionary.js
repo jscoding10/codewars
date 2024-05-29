f874db677fc313992f16d6538e29f9c4beca0e0a
jscoding10 <jscoding10@protonmail.com>
2024-05-29 05:59:31 -0700

    Added new coding challenge

---
+++ 7 kyu/interactive-dictionary.js
@@ -0,0 +1,16 @@
+// My Solution
+
+class Dictionary {
+    constructor() {
+      this.dict = {};
+    }
+    
+    newEntry(key, value) {
+      this.dict[key] = value;
+    }
+    
+    look(key) {
+      return this.dict[key] || 
+      `Can\'t find entry for ${key}`;
+    }
+  }
