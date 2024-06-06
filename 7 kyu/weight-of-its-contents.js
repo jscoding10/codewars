93bb0875c9bf0ad72bb46813490891b4280cc84c
jscoding10 <jscoding10@protonmail.com>
2024-06-06 06:03:51 -0700

    Added new coding challenge

---
+++ 7 kyu/weight-of-its-contents.js
@@ -0,0 +1,5 @@
+// My Solution
+function contentWeight(bottleWeight, scale) {
+    var s = parseInt(scale);
+    return /larger/.test(scale) ? bottleWeight * s / (s + 1) : bottleWeight / (s + 1);
+}
