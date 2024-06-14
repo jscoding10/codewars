abcd15f07e32aafc54d0d713c1efcb57dd5bff17
jscoding10 <jscoding10@protonmail.com>
2024-06-14 11:38:33 -0700

    Added new coding challenge

--- 7 kyu/node-intro.js
+++ 7 kyu/node-intro.js
@@ -9,7 +9,7 @@ The runtime environment gives your code the ability to interact with software ou
 
 Node.js, however, cannot use those browser-specific built-ins (go ahead, try). Node.js is an environment for javascript that is meant to run on your computer (server-side). So while you lose the ability to work with browser built-ins, you gain access to NodeJS-specific code.
 
-Kata
+Kata 
 We are going to replicate the kata Base64 Encoding, but instead of creating our own encoding / decoding functions, we will use NodeJS' Buffer module to do it for us.
 
 Create the function String.prototype.toBase64 that encodes a string in Base64. Also create the function String.prototype.fromBase64 that decodes a Base64 string into utf8.
