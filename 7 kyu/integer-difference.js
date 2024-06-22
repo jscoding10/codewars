78776b7a57beb0a8874f40281ec966728a90670a
jscoding10 <jscoding10@protonmail.com>
2024-06-22 14:32:55 -0700

    Added new coding challenge

--- 7 kyu/integer-difference.js
+++ 7 kyu/integer-difference.js
@@ -4,7 +4,7 @@ Write a function that accepts two arguments: an array of integers and another in
 
 Determine the number of times where two integers in the array have a difference of n.
 
-For example:
+For example: 
 
 int_diff([1, 1, 5, 6, 9, 16, 27], 4) # 3 ([1, 5], [1, 5], [5, 9])
 int_diff([1, 1, 3, 3], 2) # 4 ([1, 3], [1, 3], [1, 3], [1, 3])
