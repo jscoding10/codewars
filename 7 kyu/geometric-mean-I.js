80ccaafbf69a57191dd6343909323da42c4d9190
jscoding10 <jscoding10@protonmail.com>
2024-07-23 06:08:35 -0700

    Edited coding challenge

--- 7 kyu/geometric-mean-I.js
+++ 7 kyu/geometric-mean-I.js
@@ -2,7 +2,7 @@
 For a variable, x, that may have different values, the geometric mean is defined as:
 
 Suposse that you have to calculate the geometric mean for a research where the amount of values of x is rather small.
-
+ 
 Implement the function geometric_meanI(), (geometricMeanI javascript)that receives an array with the different values of the variable and outputs the geometric mean value.
 
 The negative values and strings will be discarded for the calculations.
