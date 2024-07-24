c0bc733f26415cee51d68a8852e93e078ec58956
jscoding10 <jscoding10@protonmail.com>
2024-07-24 06:10:04 -0700

    Edited coding challenge

--- 7 kyu/integer-difference.js
+++ 7 kyu/integer-difference.js
@@ -9,7 +9,7 @@ For example:
 int_diff([1, 1, 5, 6, 9, 16, 27], 4) # 3 ([1, 5], [1, 5], [5, 9])
 int_diff([1, 1, 3, 3], 2) # 4 ([1, 3], [1, 3], [1, 3], [1, 3])
 */
-
+ 
 // My Solution
 const intDiff = (arr, n) => {
     let count = 0;
