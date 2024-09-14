acd26197f104f39ca15a3c867982048e236f079e
jscoding10 <jscoding10@protonmail.com>
2024-09-14 11:29:45 -0700

    Edited coding challenge

--- 7 kyu/number-climber.js
+++ 7 kyu/number-climber.js
@@ -1,5 +1,5 @@
 /*
-Description:
+Description: 
 For every positive integer N, there exists a unique sequence starting with 1 and ending with N and such that every number in the sequence is either the double of the preceeding number or the double plus 1.
 
 For example, given N = 13, the sequence is [1, 3, 6, 13], because . . . :
