2acfb45dc7d35ecb98493e58e70e978919c94a1d
jscoding10 <jscoding10@protonmail.com>
2024-10-03 07:07:26 -0700

    Edited coding challenge

--- 7 kyu/check-digit.js
+++ 7 kyu/check-digit.js
@@ -35,4 +35,4 @@ Find more details below:
 function checkDigit(number, index1, index2, digit) {
     if(index1 > index2) { [index1, index2] = [index2, index1]}
     return (''+number).slice(index1, index2+1).indexOf(''+digit) > -1;
-} 
+}  
