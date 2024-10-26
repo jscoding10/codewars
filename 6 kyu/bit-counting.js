0e34d7a5f06ad36a9687ca41b3e304907c4f362a
jscoding10 <jscoding10@protonmail.com>
2024-10-26 13:15:08 -0700

    Edited coding challenge

--- 6 kyu/bit-counting.js
+++ 6 kyu/bit-counting.js
@@ -5,7 +5,7 @@ Write a function that takes an integer as input, and returns the number of bits 
 Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
 */
  
-// My Solution
+// My Solution 
 var countBits = function(n) {
     return n.toString(2).replace(/0/g,'').length;
 }; 
