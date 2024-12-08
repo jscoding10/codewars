793cce134bce91edc720e3213833177f17bd4279
jscoding10 <jscoding10@protonmail.com>
2024-12-07 17:47:20 -0800

    Edited coding challenge

--- 6 kyu/steps-in-primes.js
+++ 6 kyu/steps-in-primes.js
@@ -8,7 +8,7 @@ function step(g, m, n){
 function isPrime(n) {
   let rt = Math.sqrt(n);
   for(let i = 2; i <= rt; i++) {
-    if(n % i === 0) return false; 
+    if(n % i === 0) return false;  
   }
   return n !== 1;
 }  
