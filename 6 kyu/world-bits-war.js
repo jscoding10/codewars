313ce20972e6fc407c53e660b6f202692771d816
jscoding10 <jscoding10@protonmail.com>
2024-12-22 22:37:27 -0800

    Edited coding challenge

--- 6 kyu/world-bits-war.js
+++ 6 kyu/world-bits-war.js
@@ -6,4 +6,4 @@ function bitsWar(numbers) {
     let even = numbers.filter(v => v % 2 === 0)
     let even2 = even.map(v => Math.abs(v).toString(2).replace(/0/g,'').length).map((v, i) => even[i] < 0 ? v *- 1 : v * 1).reduce((a, b) => a + b, 0)
     return odd2 > even2 ? 'odds win' : odd2 === even2 ? 'tie':'evens win'
-} 
+}  
