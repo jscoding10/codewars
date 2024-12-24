4e835b3a0995fc04184f3faf1d0d4d917557b60d
jscoding10 <jscoding10@protonmail.com>
2024-12-24 15:29:20 -0800

    Edited coding challenge

--- 6 kyu/the-nth-smallest-integer.js
+++ 6 kyu/the-nth-smallest-integer.js
@@ -1,5 +1,5 @@
 // My Solution
 function nthSmallest(arr, n) {
-    const a = Array.from(new Set(arr)).sort((a,b) => a-b)[n-1];
-    return a === undefined ? null : a
+  const a = Array.from(new Set(arr)).sort((a, b) => a - b)[n - 1];
+  return a === undefined ? null : a;
 } 
