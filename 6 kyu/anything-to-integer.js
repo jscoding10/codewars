17ad2299f83fe1687558499693a942a7c3b6cd9e
jscoding10 <jscoding10@protonmail.com>
2024-12-30 22:09:01 -0800

    Edited coding challenge

--- 6 kyu/anything-to-integer.js
+++ 6 kyu/anything-to-integer.js
@@ -1,7 +1,7 @@
 // My Solution
 function toInteger(n) {
-    if (isNaN(n)) return 0
-    n = Math.trunc(n)
-    if (!Number.isFinite(n)) return 0
-    return n    
-}    
+  if (isNaN(n)) return 0;
+  n = Math.trunc(n);
+  if (!Number.isFinite(n)) return 0;
+  return n;
+}
