472f42d79716a7035f8a1022eafe73c6adf97db2
jscoding10 <jscoding10@protonmail.com>
2024-12-30 22:11:00 -0800

    Edited coding challenge

--- 6 kyu/array-diff.js
+++ 6 kyu/array-diff.js
@@ -8,9 +8,9 @@ array_diff([1,2],[1]) == [2]
 If a value is present in b, all of its occurrences must be removed from the other:
 
 array_diff([1,2,2,2,3],[2]) == [1,3]   
-*/ 
+*/
 
 // My Solution
 function array_diff(a, b) {
-    return a.filter(e => !b.includes(e));  
-}   
+  return a.filter((e) => !b.includes(e));
+}
