b437848db04f3a9c9cf8af8af59b107396353500
jscoding10 <jscoding10@protonmail.com>
2025-01-28 07:20:19 -0800

    Edited coding challenge

--- 6 kyu/the-freeway-game.js
+++ 6 kyu/the-freeway-game.js
@@ -4,4 +4,4 @@ var freewayGame = function(distKmToExit, mySpeedKmPH, otherCars) {
     return otherCars.reduce((a, [lead, speed]) => a
         + (lead < 0 && myTimeToExit * speed < distKmToExit + lead / 60 * speed)
         - (lead > 0 && myTimeToExit * speed > distKmToExit + lead / 60 * speed), 0);
-}    
+}     
