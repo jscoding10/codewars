7e6629a7b5c62c16c42631658136de5fccf0a076
jscoding10 <jscoding10@protonmail.com>
2025-01-29 07:25:28 -0800

    Edited coding challenge

--- 6 kyu/balance-the-array.js
+++ 6 kyu/balance-the-array.js
@@ -15,4 +15,4 @@ function balance (arr1, arr2){
     let f = arr1.reduce((a, b) => (a[b] = a[b] + 1 | 0 + 1, a),{})
     let s = arr2.reduce((a, b) => (a[b] = a[b] + 1 | 0 + 1,a),{})
     return Object.values(f).sort().toString() === Object.values(s).sort().toString()
-  }  
+  }   
