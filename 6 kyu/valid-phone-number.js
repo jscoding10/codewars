ea0ee560c4365fb179944f8c5ff967dfb62ef9de
jscoding10 <jscoding10@protonmail.com>
2025-05-20 07:24:56 -0700

    Edited coding challenge one

--- 6 kyu/valid-phone-number.js
+++ 6 kyu/valid-phone-number.js
@@ -1,4 +1,4 @@
 // My Solution
 function validPhoneNumber(phoneNumber) {
     return (/^\(\d{3}\)\ \d{3}\-\d{4}$/).test(phoneNumber)
-}                                        
+}                                         
