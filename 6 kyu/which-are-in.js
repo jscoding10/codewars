9bc03d4ba68dbc75dd231197e630fdeda8fed571
Jacob Staten <jscoding10@protonmail.com>
2025-10-22 21:33:22 -0700

    Edited coding challenge one

--- 6 kyu/which-are-in.js
+++ 6 kyu/which-are-in.js
@@ -1,5 +1,4 @@
 // My Solution
 function inArray(array1, array2) {
   return array1.filter((x) => array2.some((y) => y.includes(x))).sort()
-}     
-                                                                            
+}                                                                      
